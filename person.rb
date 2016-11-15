d14d005688a18a7f2afc4773f4a2e38650124460
Tom Hogan <thogan@spartaglobal.com>
2016-11-15 11:45:56 +0000

    subtract method written and refactored

--- person.rb
+++ person.rb
@@ -1,3 +1,7 @@
 def add_numbers(a,b)
 	a+b
 end
+
+def sub_numbers (a,b)
+	a-b
+end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,5 +1,18 @@
-describe 'adding machine' do
+describe 'arithmetic machine' do
 	it "should add two numbers" do
 		expect(add_numbers(5,6)).to eq 11
 	end
+
+	it "should subtract two numbers" do
+		expect(sub_numbers(9,2)).to eq 7
+	end
+=begin
+	it "should add an array of numbers" do
+		expect(add_array_numbers([5,6,10])).to eq 21
+	end
+
+	it "should add an array of numbers" do
+		expect(add_array_numbers([])).to eq 0
+	end
+=end
 end
