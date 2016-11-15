4a39ca74ccdcc1b0b15cb72b3de1257a8e5c9448
Tom Hogan <thogan@spartaglobal.com>
2016-11-15 11:57:12 +0000

    array addition code added'

--- person.rb
+++ person.rb
@@ -2,6 +2,14 @@ def add_numbers(a,b)
 	a+b
 end
 
-def sub_numbers (a,b)
+def sub_numbers(a,b)
 	a-b
+end
+
+def add_array_numbers(a)
+	result = 0
+	a.each do |num|
+		result += num
+	end
+	return result
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -6,11 +6,11 @@ describe 'arithmetic machine' do
 	it "should subtract two numbers" do
 		expect(sub_numbers(9,2)).to eq 7
 	end
-=begin
+
 	it "should add an array of numbers" do
 		expect(add_array_numbers([5,6,10])).to eq 21
 	end
-
+=begin
 	it "should add an array of numbers" do
 		expect(add_array_numbers([])).to eq 0
 	end
