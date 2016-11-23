12468256e11c25433bb40e4de5a7833fb0b5dccd
Tom Hogan <thogan@spartaglobal.com>
2016-11-23 10:34:42 +0000

    changed amazon_selscript wait element

--- amazon_selscript.rb
+++ amazon_selscript.rb
@@ -5,11 +5,11 @@ driver = Selenium::WebDriver.for :chrome
 
 driver.get 'http://amazon.com'
 
-sleep 2
+wait = Selenium::WebDriver::Wait.new(:timeout => 10)
 
 driver.find_element(id: 'twotabsearchtextbox').send_keys("Eloquent Ruby\n")
 
-sleep 2
+wait.until { driver.find_element(id: 'result_0') }
 
 span = driver.find_element(id: 'result_0').find_elements(tag_name: 'span')
 puts span[5].text
